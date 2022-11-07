e5b5d61c13d7b134277767ebcc4bbe3b387dc3b6
carlos <car.l1991@yahoo.com.br>
2022-11-07 11:06:04 -0300

    Atualizar

--- JavaScript/09 Importacao/menor_maior_valor_encontrado.js
+++ JavaScript/09 Importacao/menor_maior_valor_encontrado.js
@@ -4,21 +4,17 @@ const {gets, print} = require('./funcoes_auxiliares');
 /* Encontra o maior e o menor valor 
 */
 
-
-let maiorValorEncontrado = gets();
-let menorValorEnconstado = gets();
-const valoresSorteados = gets();
-//let menorValorEncontrado = gets();
+let maiorValorEncontrado = 0;
+let menorValorEncontrado = gets();
 
 for (let i = 0; i < 5; i++){
     const numeroSorteado = gets();
     if (numeroSorteado > maiorValorEncontrado){
         maiorValorEncontrado = numeroSorteado;
-    } else if (valoresSorteados < menorValorEnconstado)
-        menorValorEnconstado = valoresSorteados; 
-   
-}
-   
+    } else {
+        menorValorEncontrado;
+    }    
+}    
 
 print('Maior valor é ' + maiorValorEncontrado);
-print('Menor valor é ' + menorValorEnconstado);
+print('Menor valor é ' + menorValorEncontrado);
